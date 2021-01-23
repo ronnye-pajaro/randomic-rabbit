3bd39c0034d1f3696ca764573192dbc29ca5ccd3
Ronnye Pajaro <77838281+ronnye-pajaro@users.noreply.github.com>
2021-01-24 00:30:57 +0100

    Added a first basic template for the web page

--- RandomicRabbitService/RandomicRabbitService/settings.py
+++ RandomicRabbitService/RandomicRabbitService/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'RandomicRabbitService.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, "RandomicRabbitService", "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
