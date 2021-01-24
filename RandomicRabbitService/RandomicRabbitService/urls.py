d94edd5aeadba3eb7432cada6384e6678734e5be
Ronnye Pajaro <77838281+ronnye-pajaro@users.noreply.github.com>
2021-01-24 01:21:34 +0100

    First working version of the service

--- RandomicRabbitService/RandomicRabbitService/urls.py
+++ RandomicRabbitService/RandomicRabbitService/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('stream/', views.stream, name='stream'),
     path('admin/', admin.site.urls),
 ]
--- RandomicRabbitService/RandomicRabbitService/views.py
+++ RandomicRabbitService/RandomicRabbitService/views.py
@@ -1,4 +1,24 @@
+from time import sleep
+from random import randint
 from django.shortcuts import render
+from django.http import StreamingHttpResponse
 
+# Generation range, both for time and value
+MIN_TIME = 1
+MAX_TIME = 5
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+# View to provide main web page, index.html
 def index(request):
     return render(request, 'index.html', {})
+
+# Random numbers stream
+def stream(request):
+    def event_stream():
+        while True:
+            v = randint(MIN_VALUE, MAX_VALUE)
+            yield 'data: %s\n\n' % v
+            t = randint(MIN_TIME, MAX_TIME)
+            sleep(t)
+    return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
