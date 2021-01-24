fce2f44f53006cd726c35c9b39370efe6c1a0f5a
Ronnye Pajaro <77838281+ronnye-pajaro@users.noreply.github.com>
2021-01-24 19:29:05 +0100

    Given a better look with Bootstrap

--- RandomicRabbitService/RandomicRabbitService/settings.py
+++ RandomicRabbitService/RandomicRabbitService/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "RandomicRabbitService" / "static",
+]
+print("---> ")
+print(STATICFILES_DIRS)
